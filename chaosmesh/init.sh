5302c8b52f28912743a2cb510c1eb176b34b4cc7
Rajesh Dutta <you@example.com>
2023-01-18 15:20:01 +0100

    repalced docker with containerd

--- chaosmesh/init.sh
+++ chaosmesh/init.sh
@@ -2,6 +2,4 @@ helm repo add chaos-mesh https://charts.chaos-mesh.org
 helm search repo chaos-mesh
 kubectl create ns chaos-mesh
 # Default to /var/run/docker.sock
-#helm upgrade --install chaos-mesh chaos-mesh/chaos-mesh -n=chaos-mesh --version 2.5.1
-helm upgrade --install chaos-mesh chaos-mesh/chaos-mesh -n=chaos-mesh --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/k3s/containerd/containerd.sock --version 2.5.1
-
+helm upgrade --install chaos-mesh chaos-mesh/chaos-mesh -n=chaos-mesh --version 2.5.1 --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/containerd/containerd.sock
