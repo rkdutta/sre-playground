164de3fa225cffdafeee456d66c89b2b66acb939
Rajesh Dutta <you@example.com>
2023-01-23 13:10:28 +0100

    added loki

--- install-loki.sh
+++ install-loki.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+kubectl create ns loki-stack
 helm repo add loki https://grafana.github.io/helm-charts
 helm upgrade --install loki loki/loki-stack \
   --namespace=loki-stack \
   --create-namespace \
   --atomic \
-  --values loki/loki-stack-values.yaml \
+  --values loki/values.yaml \
   --wait
-
-kubectl get secret --namespace loki-stack loki-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
 
