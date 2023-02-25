4c0538fd3939904c76df147c244e600fe948bce8
Rajesh Dutta <you@example.com>
2023-02-25 20:03:15 +0100

    added prometheus

---
+++ install-prometheus.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# install prometheus operator
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm upgrade --install \
+ prometheus prometheus-community/kube-prometheus-stack \
+ --dependency-update \
+ --atomic \
+ --namespace prometheus \
+ --create-namespace \
+ --values prometheus/values.yaml 
+
+echo "`date` >>>>> prometheus started..."
