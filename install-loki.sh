9a53b0a3fe61f4aacdb46cc30bfd36816770e045
Rajesh Dutta <you@example.com>
2023-02-25 20:52:10 +0100

    Finalized loki

--- install-loki.sh
+++ install-loki.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-kubectl create ns loki-stack
 helm repo add loki https://grafana.github.io/helm-charts
 helm upgrade --install loki loki/loki-stack \
   --namespace=loki \
   --create-namespace \
-  --atomic \
-  --values loki/values.yaml \
-  --wait
-
+  --values loki/values.yaml 
