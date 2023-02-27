1c6a63fe08de571d2b69a6d54b912e3b1105321b
Rajesh Dutta <you@example.com>
2023-02-27 10:34:53 +0100

    updated loki installer

--- install-loki.sh
+++ install-loki.sh
@@ -5,3 +5,9 @@ helm upgrade --install loki loki/loki-stack \
   --namespace=loki \
   --create-namespace \
   --values loki/values.yaml 
+
+echo "`date` >>>>> wait for loki to be ready"
+kubectl wait --namespace loki \
+                --for=condition=ready pod \
+                --selector=app=loki \
+                --timeout=90s
