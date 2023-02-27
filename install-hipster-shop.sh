8114e893ba4d8b958796d8b98520459be45c3e7a
Rajesh Dutta <you@example.com>
2023-02-27 10:33:32 +0100

    hipster-shop app values updated

--- install-hipster-shop.sh
+++ install-hipster-shop.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+namespace="default"
+app="hipster-shop"
+selector="app.kubernetes.io/name=hipster-shop"
+
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm upgrade --install \
- hipster-shop open-telemetry/opentelemetry-demo \
- --namespace "default" \
+ $app open-telemetry/opentelemetry-demo \
+ --namespace $namespace \
  --create-namespace \
  --values apps/hipster-shop-app/values.yaml
+
+echo "`date` >>>>> wait for $app to be ready"
+kubectl wait --namespace $namespace \
+                --for=condition=ready pod \
+                --selector=$selector \
+                --timeout=90s
