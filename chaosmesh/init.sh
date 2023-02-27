f7b2241f42a76a9e29b62644c88329d28fd21929
Rajesh Dutta <you@example.com>
2023-02-27 11:48:29 +0100

    deleted init.sh

--- chaosmesh/init.sh
+++
@@ -1,5 +0,0 @@
-helm repo add chaos-mesh https://charts.chaos-mesh.org
-helm search repo chaos-mesh
-kubectl create ns chaos-mesh
-# Default to /var/run/docker.sock
-helm upgrade --install chaos-mesh chaos-mesh/chaos-mesh -n=chaos-mesh --version 2.5.1 --set chaosDaemon.runtime=containerd --set chaosDaemon.socketPath=/run/containerd/containerd.sock
