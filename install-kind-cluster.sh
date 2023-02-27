8b31ca0ba9f80cbdf69a9c9999f01a0a050fe5f0
Rajesh Dutta <you@example.com>
2023-02-27 10:32:43 +0100

    Updated kind config and installer

--- install-kind-cluster.sh
+++ install-kind-cluster.sh
@@ -4,8 +4,10 @@
 # Set the Virtual disk limit: 100 GB
 # For better performance set the memory: 20 GB
 
-USE_LOCAL_IMAGES=true
-FORCE_BUILD_IMAGES_LOCALLY=true
+clear
+
+USE_LOCAL_IMAGES=false
+FORCE_BUILD_IMAGES_LOCALLY=false
 ClusterName="sre-demo-site"
 KIND_CONFIG_FILE="kind/kind-config-NoCNI.yaml"
 DEMO_DIR="opentelemetry-demo"
@@ -58,33 +60,45 @@ kubectl config use-context kind-$ClusterName
 
 
 # checking kubernetes status
-until kubectl get sa default 2>&1 > /dev/null; do
-  echo "Waiting for Kubernetes to start..."
-  sleep 1
-done
-echo " `date` >>>>> kubernetes started..."
+# until kubectl get sa default 2>&1 > /dev/null; do
+#   echo "Waiting for Kubernetes to start..."
+#   sleep 1
+# done
+# echo " `date` >>>>> kubernetes started..."
+# Checking if apiserver, etcd, controller-manager, scheduler is running
+
+echo "`date` >>>>> wait for controle-plane components to be ready"
+kubectl wait --namespace kube-system \
+  --for=condition=ready pod \
+  --selector=tier=control-plane \
+  --timeout=90s
+
 
 # Install CNI: Cilium
-sh install-cilium.sh $ClusterName
+EnableHubbleUISwitch=false
+sh install-cilium.sh $ClusterName $EnableHubbleUISwitch
 
 # verifying if coreDNS pods are up and running
-until kubectl -n kube-system describe deployments.apps coredns | grep "0 unavailable"; do
-  kubectl -n kube-system rollout restart deployment coredns
-  echo "Waiting for coredns pods to start.."
-  sleep 20
-done
-echo "`date` >>>>> coreDNS started..."
-echo "`date` >>>>> cluster created successfully..."
+# until kubectl -n kube-system describe deployments.apps coredns | grep "0 unavailable"; do
+#   kubectl -n kube-system rollout restart deployment coredns
+#   echo "Waiting for coredns pods to start.."
+#   sleep 20
+# done
+# echo "`date` >>>>> coreDNS started..."
+# echo "`date` >>>>> cluster created successfully..."
 
-# verify Masquerading
-kubectl -n kube-system exec ds/cilium -- cilium status | grep Masquerading
+echo "`date` >>>>> wait for kube-dns to be ready"
+kubectl wait --namespace kube-system \
+  --for=condition=ready pod \
+  --selector=k8s-app=kube-dns \
+  --timeout=90s
 
 
 # transporting the locally build docker images to kind nodes
 for i in "${DEMO_SERVICES[@]}"
 do
-   echo ">> $CONTAINER_REGISTRY:$RELEASE_VERSION-$i"
    if $USE_LOCAL_IMAGES ; then
+    echo ">> $CONTAINER_REGISTRY:$RELEASE_VERSION-$i"
     kind load docker-image  $CONTAINER_REGISTRY:$RELEASE_VERSION-$i --name $ClusterName
    fi
 done
@@ -93,9 +107,24 @@ done
 #install loki
 sh install-loki.sh
 
+#install fluent-bit
+sh install-fluent-bit.sh
+
+#install prometheus
+sh install-prometheus.sh
+
+#install ingress controller
+sh install-ingress.sh
+
 #install hipster application
 sh install-hipster-shop.sh
 
+# #install metallb
+# sh metallb/install-metalb.sh
+
+# # Install CNI: Enable Hubble ui
+# EnableHubbleUISwitch=false
+# sh install-cilium.sh $ClusterName $EnableHubbleUISwitch
 
 
 echo "SUCCESS.."
