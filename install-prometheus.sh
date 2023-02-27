4d99e7ed66dbc564ff648084dac1022decde5724
Rajesh Dutta <you@example.com>
2023-02-27 10:35:09 +0100

    updated prometheus operator

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -4,10 +4,20 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm upgrade --install \
  prometheus prometheus-community/kube-prometheus-stack \
- --dependency-update \
- --atomic \
  --namespace prometheus \
  --create-namespace \
  --values prometheus/values.yaml
 
-echo "`date` >>>>> prometheus started..."
+echo "`date` >>>>> wait for prometheus to be ready"
+kubectl wait --namespace prometheus \
+  --for=condition=ready pod \
+  --selector=release=prometheus \
+  --timeout=90s
+kubectl wait --namespace prometheus \
+  --for=condition=ready pod \
+  --selector="app.kubernetes.io/instance=prometheus" \
+  --timeout=90s
+kubectl wait --namespace prometheus \
+  --for=condition=ready pod \
+  --selector="app.kubernetes.io/name=alertmanager" \
+  --timeout=90s
