591bcca99e726113608e815a8fc4530732d282db
Rajesh Dutta <you@example.com>
2023-02-27 10:28:02 +0100

    added ingress rules

--- install-ingress.sh
+++ install-ingress.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-NAMESPACE=ingress
-
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-helm repo update
-
-helm install ingress-nginx ingress-nginx/ingress-nginx \
-  --create-namespace \
-  --namespace $NAMESPACE \
-  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-health-probe-request-path"=/healthz
+kubectl apply -f ingress/nginx-ingress-def.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+kubectl apply -f ingress/ingress-rules.yaml
