d4c5201920ef5216981df54cfd9535dd19407021
Rajesh Dutta <you@example.com>
2023-02-27 10:34:39 +0100

    updated cilium

--- install-cilium.sh
+++ install-cilium.sh
@@ -1,25 +1,28 @@
 ClusterName=$1
+EnableHubbleSwitch=$2
+
+ClusterName=sre-demo-site-control-plane
+EnableHubbleSwitch=true
+namespace="kube-system"
+
+#LB_IP=$(kubectl get svc -n ingress-nginx ingress-nginx-controller -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 helm repo add cilium https://helm.cilium.io/
 helm upgrade \
     --install cilium cilium/cilium \
-    --namespace kube-system \
-    --set image.pullPolicy=IfNotPresent \
-    --set ipam.mode=kubernetes \
-    --set kubeProxyReplacement=strict \
-    --set k8sServiceHost=$ClusterName-control-plane \
-    --set k8sServicePort=6443 \
-    --set bpf.masquerade=true \
-    --set hubble.relay.enabled=true \
-    --set hubble.ui.enabled=true \
-    --set hubble.tls.auto.enabled=true \
-    --set hubble.tls.auto.method=helm \
-    --reuse-values \
-    --set ipMasqAgent.enabled=true \
-    --set prometheus.enabled=true \
-    --set operator.prometheus.enabled=true \
-    --set hubble.enabled=true \
-    --set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,http}"
+    --namespace $namespace \
+    --values cilium/values.yaml \
+    --set k8sServiceHost=$ClusterName
+
+   # --set hubble.ui.enabled=$EnableHubbleSwitch \
+   # --set hubble.ui.ingress.hosts[0]="hubble-ui.${LB_IP}.nip.io" \
+
+echo "`date` >>>>> wait for cilium to be ready"
+kubectl wait --namespace $namespace \
+                --for=condition=ready pod \
+                --selector=k8s-app=cilium \
+                --timeout=90s
 
 # verify Masquerading
 kubectl -n kube-system exec ds/cilium -- cilium status | grep Masquerading
+
