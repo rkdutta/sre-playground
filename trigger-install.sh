d7c46e54caf54613fe1aff755c9885c7a86f969c
Rajesh Dutta <you@example.com>
2023-03-06 15:13:59 +0100

    updated installation script

--- trigger-install.sh
+++ trigger-install.sh
@@ -22,17 +22,6 @@ waitForReadiness(){
                 --timeout=90s
 }
 
-installer(){
-  local_app=$1
-  local_namespace=$2
-  local_clusterName=$3
-  local_selector=$4
-  (
-    cd $local_app && \
-    ./installer.sh $*
-  )
-  waitForReadiness $local_app $local_namespace $local_selector
-}
 # ///////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -42,7 +31,7 @@ installer(){
 # ///////////////////////////////////////////////////////////////////////////////////////
 
 ENABLE_KUBE_PROXY=${1:-true} # when false cilium will be installed and kube-proxy will be disabled
-USE_LOCAL_IMAGES=${2:-false} # When true docker compose will run and create local images
+USE_LOCAL_IMAGES=${2:-true} # When true docker compose will run and create local images
 RELEASE_VERSION="1.3.0" # Open Telemetry Community Demo Version. Check: https://github.com/open-telemetry/opentelemetry-demo
 CLUSTER_NAME=${3:-"sre-demo-site"} # kind cluster name
 DEMO_DIR="opentelemetry-demo" 
@@ -106,29 +95,6 @@ selector="tier=control-plane"
 namespace="kube-system"
 waitForReadiness $app $namespace $selector
 
-
-if ! $ENABLE_KUBE_PROXY ; then
-  # Install CNI: Cilium
-  app="cilium"
-  selector="k8s-app=cilium"
-  namespace="kube-system"
-  installer $app $namespace $CLUSTER_NAME $selector
-  else
-  # verify deefault CNI installation
-  app="cni-kindnet"
-  selector="app=kindnet"
-  namespace="kube-system"
-  waitForReadiness $app $namespace $selector
-fi
-
-# verifying cluster installation 
-app="kube-dns"
-selector="k8s-app=kube-dns"
-namespace="kube-system"
-waitForReadiness $app $namespace $selector
-kubectl get pods -A
-
-
 # transporting the locally build docker images to kind nodes
 for i in "${DEMO_SERVICES[@]}"
 do
@@ -138,65 +104,9 @@ do
    fi
 done
 
-
-#install loki
-app="loki"
-selector="app=loki"
-namespace="loki"
-installer $app $namespace $CLUSTER_NAME $selector
-
-#install fluent-bit
-app="fluent-bit"
-selector="app.kubernetes.io/instance=fluent-bit"
-namespace="fluent-bit"
-installer $app $namespace $CLUSTER_NAME $selector
-
-
-#install prometheus
-app="prometheus"
-selector="release=prometheus"
-namespace="prometheus"
-installer $app $namespace $CLUSTER_NAME $selector
-selector="app.kubernetes.io/instance=prometheus"
-waitForReadiness $app $namespace $selector
-selector="app.kubernetes.io/name=alertmanager"
-waitForReadiness $app $namespace $selector
-selector="app.kubernetes.io/managed-by=prometheus-operator"
-waitForReadiness $app $namespace $selector
-
-
-#install hipster application
-app="app-hipster-shop"
-selector="app.kubernetes.io/name=app-hipster-shop"
-namespace="default"
-installer $app $namespace $CLUSTER_NAME $selector
-
-
-#install metallb
-( cd metallb &&  ./installer.sh )
-
-# # Install CNI: Enable Hubble ui
-# EnableHubbleUISwitch=false
-# sh install-cilium.sh $CLUSTER_NAME $EnableHubbleUISwitch
-
-
-#install ingress controller
-app="ingress"
-selector="app.kubernetes.io/name=ingress-nginx"
-namespace="ingress"
-installer $app $namespace $CLUSTER_NAME $selector
-
-
-#install chaos-mesh and execute
-app="chaos-mesh"
-selector="app.kubernetes.io/instance=chaos-mesh"
-namespace="chaos-mesh"
-installer $app $namespace $CLUSTER_NAME $selector
-selector="app.kubernetes.io/component=chaos-daemon"
-waitForReadiness $app $namespace $selector
-
-
-# Apply the ingress rules
-kubectl apply -f ingress/ingress-rules.yaml
+helm upgrade --install --dependency-update  sreplayground-cluster sreplayground-cluster -n kube-system --create-namespace --wait
+helm upgrade --install --dependency-update  sreplayground-platform sreplayground-platform -n platform --create-namespace
+helm upgrade --install --dependency-update  sreplayground-app sreplayground-app -n app --create-namespace
 
 echo "SUCCESS.."
+
