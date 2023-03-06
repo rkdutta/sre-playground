bc8921c04277ce6b94268a978355bad69749e4ec
Rajesh Dutta <you@example.com>
2023-03-06 15:40:05 +0100

    housekeeping

--- chaos-mesh/chaos-testing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eo pipefail
-
-app=${1:-"chaos-mesh"}
-namespace=${2:-"chaos-mesh"}
-clusterName=${3:-"sre-demo-site"}
-
-#Executing chaos experiment workflow:
-kubectl apply -f workflows/chaos-workflow.yaml
