e739867bed0ce3ec1846667f7c868bf806c94dd5
Rajesh Dutta <you@example.com>
2023-03-14 17:28:24 +0100

    updated requirements file

--- requirements.sh
+++ requirements.sh
@@ -15,8 +15,8 @@ sudo apt-get update
 sudo apt-get install helm
 
 # install kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
-chmod +x ./kind
+sudo curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+sudo chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
 
